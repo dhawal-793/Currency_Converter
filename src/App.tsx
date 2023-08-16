a9d81b39135949acdb501e36edb5aeaf469c6634
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-08-16 19:17:59 +0530

    Created different methods for the app.

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,54 @@ import {
   View,
 } from 'react-native';
 
+import { currencyByRupee } from './constants';
+
 
 function App(): JSX.Element {
   const [inputValue, setInputValue] = useState('')
   const [resultValue, setResultValue] = useState('')
   const [targetCurrency, setTargetCurrency] = useState('')
   const [action, setAction] = useState('convert')
+
+  const buttonPressed = () => {
+    if (!inputValue) {
+      // TODO: show snackbar
+    }
+
+    const targetValue = currencyByRupee.find((item) => item.name === targetCurrency)
+
+    if (targetValue !== undefined) {
+      const inputAmount = parseFloat(inputValue)
+      if (!isNaN(inputAmount)) {
+        const convertedValue = inputAmount * targetValue.value
+        const result = `${targetValue.symbol} ${convertedValue.toFixed(2)}`
+        setResultValue(result)
+        setTargetCurrency(targetValue.name)
+      }
+      else {
+        // TODO: show snackbar
+      }
+    }
+    else {
+      // TODO: show snackbar
+    }
+  }
+  const handleClick = () => {
+    if (action === "convert") {
+      buttonPressed()
+      if ((inputValue && targetCurrency) || resultValue)
+        setAction("reset")
+    } else {
+      reset()
+      setAction("convert")
+    }
+  }
+
+  const reset = () => {
+    setInputValue('')
+    setResultValue('')
+    setTargetCurrency('')
+  }
 
   return (
     <SafeAreaView>
