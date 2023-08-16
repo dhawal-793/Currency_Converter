bfa6d41979adcc18c5abcd67e62c51d93a5625ee
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-08-16 19:40:20 +0530

    Added styles for the app.

--- src/App.tsx
+++ src/App.tsx
@@ -59,11 +59,11 @@ function App(): JSX.Element {
   }
 
   return (
-    <View>
-      <View>
-        <View>
-          <View>
-            <Text>₹</Text>
+    <View style={styles.container}>
+      <View style={styles.topContainer}>
+        <View style={styles.inputContainer}>
+          <View style={styles.rupeesContainer}>
+            <Text style={styles.rupee}>₹</Text>
             <TextInput
               maxLength={14}
               value={inputValue}
@@ -72,27 +72,35 @@ function App(): JSX.Element {
               keyboardType='number-pad'
               placeholder='Enter amount in Rupees'
               placeholderTextColor="#BCBBBB"
+              style={styles.input}
             />
           </View>
-          <Pressable onPress={() => handleClick()}>
-            <Text>{action}</Text>
+          <Pressable
+            onPress={() => handleClick()}
+            style={styles.button}
+          >
+            <Text style={styles.buttonText}>{action}</Text>
           </Pressable>
         </View>
-        <View>
+        <View style={styles.resultContainer}>
           {inputValue && resultValue && (
-            <Text>
+            <Text style={styles.resultTxt} >
               {`₹ ${inputValue} = `}{resultValue}
             </Text>
           )}
         </View>
       </View>
-      <View>
+      <View style={styles.bottomContainer}>
         <FlatList
           numColumns={2}
           data={currencyByRupee}
           keyExtractor={item => item.name}
           renderItem={({ item }) => (
             <Pressable
+              style={[
+                styles.card,
+                targetCurrency === item.name && styles.selected
+              ]}
               onPress={() => setTargetCurrency(currency => currency === item.name ? "" : item.name)}
             >
               <CurrencyButton {...item} />
@@ -104,6 +112,90 @@ function App(): JSX.Element {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 20,
+    backgroundColor: '#51515f',
+  },
+  topContainer: {
+    flex: 1,
+    marginTop: 80,
+    marginBottom: -80,
+    gap: 20,
+    paddingHorizontal: 40,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    height: 50,
+    gap: 20
+  },
+  rupeesContainer: {
+    flex: 1,
+    gap: 2,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+    borderWidth: 2,
+    borderRadius: 8,
+    width: 'auto',
+    paddingHorizontal: 10
+  },
+  rupee: {
+    marginRight: 8,
+    fontSize: 22,
+    color: '#000000',
+    fontWeight: '600',
+  },
+  input: {
+    height: 40,
+    fontWeight: '600',
+    color: '#000000',
+  },
+  button: {
+    flexDirection: 'row',
+    backgroundColor: '#FFFFFF',
+    alignItems: 'center',
+    borderRadius: 8,
+    height: 40,
+    width: 80,
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: '#000000',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  resultContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    borderColor: '#ffffff',
+    height: 70,
+  },
+  resultTxt: {
+    textAlign: 'center',
+    fontSize: 32,
+    color: '#ffffff',
+    fontWeight: '700',
+  },
+  bottomContainer: {
+    flex: 2,
+    paddingHorizontal: 30,
+  },
+  card: {
+    flex: 1,
+    margin: 12,
+    height: 80,
+    borderRadius: 12,
+    backgroundColor: '#ffffff',
+  },
+  selected: {
+    backgroundColor: '#ffeaa7',
+  },
+});
 
 export default App;
